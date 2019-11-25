16a0509dbdc3f858876afdd150d6092283c1c74d
Bruce IB Suh <bummy815@gmail.com>
2019-11-25 16:27:26 +0900

    191125 multi image handling failed

--- app/src/main/java/com/silverback/carman2/fragments/BoardPostDialogFragment.java
+++ app/src/main/java/com/silverback/carman2/fragments/BoardPostDialogFragment.java
@@ -2,37 +2,42 @@ package com.silverback.carman2.fragments;
 
 
 import android.app.Dialog;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.LruCache;
+import android.view.Display;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.widget.Toolbar;
+import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.widget.FrameLayout;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
-
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.request.RequestOptions;
-import com.google.firebase.firestore.DocumentSnapshot;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.silverback.carman2.R;
 import com.silverback.carman2.logs.LoggingHelper;
 import com.silverback.carman2.logs.LoggingHelperFactory;
 import com.silverback.carman2.models.FirestoreViewModel;
 
+import java.net.URI;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -43,6 +48,9 @@ public class BoardPostDialogFragment extends DialogFragment {
 
     private static final LoggingHelper log = LoggingHelperFactory.create(BoardPostDialogFragment.class);
 
+    // Constants
+    private static final int IMAGE_CACHE_SIZE = 1024 * 1024 * 4;
+
 
     // Objects
     private FirebaseFirestore firestore;
@@ -50,6 +58,17 @@ public class BoardPostDialogFragment extends DialogFragment {
     private SimpleDateFormat sdf;
     private String postTitle, postContent, userName, userPic;
     private List<String> imgUriList;
+    private LruCache<String, Bitmap> memCache;
+
+    // UIs
+    private ConstraintLayout constraintLayout;
+    private ConstraintSet set;
+    private ImageView imgView;
+    private ConstraintLayout.LayoutParams layoutParams;
+
+    // Fields
+    private List<Integer> imgIdList;
+    private int prevImageViewId;
 
     public BoardPostDialogFragment() {
         // Required empty public constructor
@@ -71,6 +90,13 @@ public class BoardPostDialogFragment extends DialogFragment {
             imgUriList = getArguments().getStringArrayList("imageUriList");
         }
 
+        memCache = new LruCache<String, Bitmap>(IMAGE_CACHE_SIZE) {
+            protected int sizeOf(String key, Bitmap value) {
+                return value.getByteCount();
+            }
+        };
+
+
     }
 
 
@@ -80,7 +106,7 @@ public class BoardPostDialogFragment extends DialogFragment {
                              Bundle savedInstanceState) {
 
         View localView = inflater.inflate(R.layout.dialog_board_post, container, false);
-        ConstraintLayout constraintLayout = localView.findViewById(R.id.root_constraint);
+        constraintLayout = localView.findViewById(R.id.root_constraint);
         ImageButton btn = localView.findViewById(R.id.imgbtn_dismiss);
         TextView tvTitle = localView.findViewById(R.id.tv_post_title);
         TextView tvUserName = localView.findViewById(R.id.tv_username);
@@ -104,31 +130,110 @@ public class BoardPostDialogFragment extends DialogFragment {
                 .circleCrop()
                 .into(imgUserPic);
 
+        // If no images are transferred, just return localview not displaying any images.
+        log.i("imagUriList: %s, %s", imgUriList, imgUriList.size());
+        if(imgUriList == null || imgUriList.size() == 0) return localView;
 
-        // Attached Image(s)
+        for(String uri : imgUriList) {
+
+            Glide.with(getContext())
+                    .asBitmap()
+                    .load(Uri.parse(uri))
+                    //.diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .fitCenter()
+                    .into(new CustomTarget<Bitmap>() {
+                        @Override
+                        public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                            memCache.put(uri, resource);
+                            log.i("Image Uri: %s", memCache.get(uri));
+
+                        }
+
+                        @Override
+                        public void onLoadCleared(@Nullable Drawable placeholder) {
+
+                        }
+                    });
+        }
+
+
+            // Attached Image(s) dynamically using LayoutParams for layout_width and height and
+        // ConstraintSet to set the layout positioned in ConstraintLayout
         ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
                 ConstraintLayout.LayoutParams.MATCH_CONSTRAINT,
                 ConstraintLayout.LayoutParams.WRAP_CONTENT);
 
-        ImageView imgView = new ImageView(getContext());
-        imgView.setId(View.generateViewId());
-        imgView.setLayoutParams(layoutParams);
-        constraintLayout.addView(imgView);
-
         ConstraintSet set = new ConstraintSet();
-        set.clone(constraintLayout);
-        set.connect(imgView.getId(), ConstraintSet.TOP, R.id.tv_posting_body, ConstraintSet.BOTTOM, 100);
-        set.connect(imgView.getId(), ConstraintSet.START, R.id.root_constraint, ConstraintSet.START);
-        set.connect(imgView.getId(), ConstraintSet.END, R.id.root_constraint, ConstraintSet.END);
-        set.applyTo(constraintLayout);
+        imgIdList = new ArrayList<>();
+        prevImageViewId = -1;
 
-        Glide.with(getContext())
-                .asBitmap()
-                .load(Uri.parse(imgUriList.get(0)))
-                .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
-                .fitCenter()
-                .into(imgView);
+        for(int i = 0; i < imgUriList.size(); i++) {
 
+            ImageView imgView = new ImageView(getContext());
+            imgView.setId(View.generateViewId());
+            imgView.setLayoutParams(layoutParams);
+            log.i("image view id: %s", imgView.getId());
+            constraintLayout.addView(imgView);
+            set.clone(constraintLayout);
+
+            set.connect(imgView.getId(), ConstraintSet.START, R.id.root_constraint, ConstraintSet.START, 50);
+            set.connect(imgView.getId(), ConstraintSet.END, R.id.root_constraint, ConstraintSet.END, 50);
+            //set.connect(imgView.getId(), ConstraintSet.TOP, R.id.tv_posting_body, ConstraintSet.BOTTOM, 50);
+            /*
+            int constrained = (i == 0)? R.id.tv_posting_body : prevImageViewId;
+            set.connect(imgView.getId(), ConstraintSet.TOP, constrained, ConstraintSet.BOTTOM, 50);
+            set.applyTo(constraintLayout);
+            */
+
+            String uri = imgUriList.get(i);
+
+            Glide.with(getContext())
+                    .asBitmap()
+                    .load(Uri.parse(imgUriList.get(i)))
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .fitCenter()
+                    .into(new CustomTarget<Bitmap>() {
+                        @Override
+                        public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+
+                            Bitmap image = memCache.get(uri);
+                            log.i("Image in cache: %s", image);
+
+                            if(uri.equals(imgUriList.get(0))) {
+                                set.connect(imgView.getId(), ConstraintSet.TOP, R.id.tv_posting_body, ConstraintSet.BOTTOM, 50);
+                                set.applyTo(constraintLayout);
+
+
+                            } else {
+                                log.i("additional images");
+                                set.connect(imgView.getId(), ConstraintSet.TOP, prevImageViewId, ConstraintSet.BOTTOM, 50);
+                                set.applyTo(constraintLayout);
+                            }
+
+                            if(image != null) {
+                                imgView.setImageBitmap(image);
+                                prevImageViewId = imgView.getId();
+                                log.i("prev id: %s", prevImageViewId);
+                            } else {
+                                Glide.with(getContext())
+                                        .load(uri)
+                                        .into(imgView);
+
+                            }
+
+
+
+                        }
+
+                        @Override
+                        public void onLoadCleared(@Nullable Drawable placeholder) {
+                            log.i("onLoadCleared");
+                        }
+
+                    });
+
+            log.i("prevImageViewId: %s", prevImageViewId);
+        }
 
         return localView;
     }
