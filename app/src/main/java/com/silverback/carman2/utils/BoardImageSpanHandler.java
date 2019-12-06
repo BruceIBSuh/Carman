dc886214013b53572f7451ebd6e55abd21b869e3
Bruce IB Suh <bummy815@gmail.com>
2019-12-06 18:16:19 +0900

    191206 Post spannable not working Damn it

--- app/src/main/java/com/silverback/carman2/utils/BoardImageSpanHandler.java
+++ app/src/main/java/com/silverback/carman2/utils/BoardImageSpanHandler.java
@@ -1,5 +1,6 @@
 package com.silverback.carman2.utils;
 
+import android.app.Notification;
 import android.content.IntentFilter;
 import android.text.Editable;
 import android.text.InputFilter;
@@ -25,7 +26,8 @@ public class BoardImageSpanHandler implements SpanWatcher {
     // Objects
     private Editable editable;
     private ImageSpan[] arrImgSpan;
-    private int cursor;
+    private int cursorPos;
+    private boolean cursorDir;
 
     // Initialize the static members
     static {
@@ -37,51 +39,61 @@ public class BoardImageSpanHandler implements SpanWatcher {
     // Constructor
     public BoardImageSpanHandler(Editable editable) {
         this.editable = editable;
-        editable.setSpan(this, 0, editable.length(), Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+        editable.setSpan(this, 0, 0, Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
     }
 
     // Callbacks invoked by SpanWatcher
     @Override
-    public void onSpanAdded(Spannable text, Object what, int start, int end) {}
+    public void onSpanAdded(Spannable text, Object what, int start, int end) {
+        //log.i("onSpanAdded");
+    }
     @Override
-    public void onSpanRemoved(Spannable text, Object what, int start, int end) {}
+    public void onSpanRemoved(Spannable text, Object what, int start, int end) {
+        //log.i("onSpanRemoved");
+    }
     @Override
     public void onSpanChanged(Spannable text, Object what, int ostart, int oend, int nstart, int nend) {
 
+        cursorDir = ostart <= nstart;
         log.i("what: %s", what);
-        // Set the selection anchor to Spannable.
-        if (what == Selection.SELECTION_START) {
-            log.i("SELECTION_START: %s, %s, %s, %s, %s", text, ostart, oend, nstart, nend);
-            cursor = (ostart == nstart)?ostart : Math.max(ostart, nstart);
 
+        // Set the selection anchor to SpannableS
+        if (what == Selection.SELECTION_START) {
+            //log.i("SELECTION_START: %s, %s, %s, %s, %s", text, ostart, oend, nstart, nend);
+            // forward direction set to true. In case of, ostart == nstart(or oend == nend), it
+            // indicates a character is inserted or deleted.
+            //log.i("Cursor direction: %s, %s", cursorDir, nend);
         // Cursor moves at the counter-direction
         } else if (what == Selection.SELECTION_END) {
-            log.i("SELECTION_END %s, %s, %s, %s, %s", text, ostart, oend, nstart, nend);
-            cursor = (ostart == nstart)?ostart : Math.max(ostart, nstart);
+            //log.i("SELECTION_END %s, %s, %s, %s, %s", text, ostart, oend, nstart, nend);
+
         }
+
+
+
+
     }
 
     // InputFilter
     public void setImageSpanInputFilter(){
+
         editable.setFilters(new InputFilter[]{ (source, start, end, dest, dstart, dend) -> {
             log.i("Filters: %s ,%s, %s, %s, %s, %s", source, start, end, dest, dstart, dend);
 
-            // Disable ImageSpan to be deleted by soft input
-            for(ImageSpan span : arrImgSpan) {
-                log.i("span position: %s, %s", dest.getSpanStart(span), dest.getSpanEnd(span));
-                if(cursor == dest.getSpanEnd(span))
-                    Selection.setSelection((Spannable)dest, dest.getSpanStart(span) - 1);
-                else if(cursor == dest.getSpanStart(span))
-                    Selection.setSelection((Spannable)dest, dest.getSpanEnd(span) + 1);
+            for(ImageSpan span : arrImgSpan){
+                log.i("span position: %s, %s, %s, %s", dstart, dend, dest.getSpanStart(span), dest.getSpanEnd(span));
+                if(dstart < dest.getSpanEnd(span)) {
+                    log.i("Skipping: %s, %s", dstart, dest.getSpanEnd(span));
+                    Selection.setSelection((Spannable)dest, dstart - markup.length());
+                }
             }
 
-            /*
             if(source instanceof Spanned) {
-                if(Math.abs(start - end) == 0 && cursor > 1)
-                    Selection.setSelection(editable, dstart - markup.length() - 1);
+                log.i("source instanceof Spanned");
             }
-            */
-            //Selection.setSelection(ssb, newStart + source.length());
+
+
+
             return null;
         }});
     }
@@ -95,7 +107,6 @@ public class BoardImageSpanHandler implements SpanWatcher {
         editable.replace(Math.min(start, end), Math.max(start, end), markup);
         editable.setSpan(span, Math.min(start, end), Math.min(start, end) + markup.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         arrImgSpan = editable.getSpans(0, editable.length(), ImageSpan.class);
-        //log.i("arrImageSpan: %s", arrImgSpan.length);
 
         Selection.setSelection(editable, editable.length());
         imageTag += 1;
